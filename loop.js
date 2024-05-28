0a09416859c2889b5d90c952d07ac10c13c8a6a0
atemo <atemotonyemao@gmail.com>
2024-05-28 13:57:34 -0700

    -m "my first vscode commit"

--- loop.js
+++ loop.js
@@ -1,3 +1,4 @@
+/*
 var greeting = 'how are you';
 console.log(greeting)
 var greeting = "how are you";
@@ -18,3 +19,11 @@ var greeting = "how are you";
 console.log(greeting);
 var greeting = "how are you";
 console.log(greeting);
+*/
+// loop is also called intration
+// there are four types of loops:while, do while, for, for each
+
+//           OR
+for (var i = 0; i <= 100; i++) {
+  console.log(`${i}. How are you?`);
+}
